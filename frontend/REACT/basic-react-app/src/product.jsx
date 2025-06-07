572269950e2e8d019943ca20d80ddb9a61ce5cbe
GarvitSingh <garvits893@gmail.com>
2025-06-07 21:21:11 +0530

    rendering object array and using conditionals

--- frontend/REACT/basic-react-app/src/product.jsx
+++ frontend/REACT/basic-react-app/src/product.jsx
@@ -9,13 +9,18 @@ import "./product.css"
 // The Product component receives props from the parent (ProductTab).
 // It uses props to display the title, description, and price of each product.
 // same as function parameters
-function Product( {title, description , price}){
+function Product( {title, description , price , features , features2}){
+
+    
     return (
         // className is a keyword for attribute class  
         <div className="product">
         <h3>{title}</h3>
         <p>{ description}</p>
         <p>price : {price}</p>
+        <p> { features.map((feature) => <li>{feature}</li>)} </p>
+        {/* add conditions ternary operator we can also use  if else */}
+        <p> Feature2: {features2?.a || "Not available"}</p>
         </div>
     );
 }
--- frontend/REACT/basic-react-app/src/productTab.jsx
+++ frontend/REACT/basic-react-app/src/productTab.jsx
@@ -9,11 +9,18 @@ import Product from "./product";
 function ProductTab(){
      // Props in React are used to pass data from parent to child components.
     // Here, we are passing props like title, description, and price to the Product component.  
+
+    // passing array or object
+    let options = ["hi-tech" , "durable" , "fast"];
+    let options2 = {a: "hi-tech" , b: "durable" , c:"fast"};
+    
     return(
     <>
-      <Product title="phone" description="phone description" price={30000} />
-      <Product title = "laptop" description="laptop description" price={40000} />
-      <Product title = "pen" description="pen description" price={5} />
+      <Product title="phone" description="phone description" price={30000} features={options} features2={options2} />
+
+      {/* we can also pass array and object directly like this  */}
+      <Product title = "laptop" description="laptop description" price={40000} features={["hi-tech" , "durable"] } features2={{a:"hi-tech" , b:"durable"}} />
+      <Product title = "pen" description="pen description" price={5} features={["fast" , "durable"]} />
 
     </>
     );
