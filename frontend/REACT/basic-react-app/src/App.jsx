033cbe6a883bdd0ada24f8383af837194910b97f
GarvitSingh <garvits893@gmail.com>
2025-06-07 11:03:25 +0530

    react basics

---
+++ frontend/REACT/basic-react-app/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css"
+import Title from "./Title.jsx"
+
+
+function Description()
+{
+  return <h3>I am a description</h3>
+}
+function App() {
+  
+
+  return(
+    <div>
+    <Title/>
+    <Description/>
+    <Title/>
+    <Description/>
+    </div>);
+}
+
+export default App
