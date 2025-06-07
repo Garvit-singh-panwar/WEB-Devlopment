90acb73ba89c0e9a0fe41d6d049fcc666f562f24
GarvitSingh <garvits893@gmail.com>
2025-06-07 11:35:24 +0530

    add style in component

--- frontend/REACT/basic-react-app/src/product.jsx
+++ frontend/REACT/basic-react-app/src/product.jsx
@@ -1,8 +1,14 @@
 // Make  a individual component 
 
+
+//  in html to use css we use tags but in react we have to import css file
+// in react there is something called webpack which makes import export functionality  possible in react
+import "./product.css"
+
 function Product(){
     return (
-        <div>
+        // className is a keyword for attribute class  
+        <div className="product">
         <h3>Product Title</h3>
         <p>Product description</p>
         </div>
